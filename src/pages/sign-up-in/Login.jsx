6431405fb689915633f7a2e5cbcf74f704d2111c
FahimCC <ffnasif@gmail.com>
2023-05-03 22:41:40 +0600

    completed login & register page

--- src/pages/sign-up-in/Login.jsx
+++ src/pages/sign-up-in/Login.jsx
@@ -1,9 +1,58 @@
+import Lottie from 'lottie-react';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import login from '../../assets/Login.json';
 
 const SignIn = () => {
 	return (
-		<div>
-			
+		<div className='hero'>
+			<div className='hero-content flex-col lg:flex-row gap-20'>
+				<div>
+					<Lottie className='h-96' animationData={login} loop={true} />
+				</div>
+				<div className='card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
+					<div className='card-body'>
+						<div className='form-control'>
+							<label className='label'>
+								<span className='label-text'>Email</span>
+							</label>
+							<input
+								type='text'
+								placeholder='email'
+								className='input input-bordered'
+								name='email'
+								required
+							/>
+						</div>
+						<div className='form-control'>
+							<label className='label'>
+								<span className='label-text'>Password</span>
+							</label>
+							<input
+								type='text'
+								placeholder='password'
+								className='input input-bordered'
+								name='password'
+								required
+							/>
+						</div>
+						<div className='form-control mt-6'>
+							<button className='btn btn-primary'>Login</button>
+						</div>
+						<div className='mt-3'>
+							<p>
+								Don't have an account?{' '}
+								<Link
+									className='Active transform active:scale-90 transition-transform'
+									to='/register'
+								>
+									Create
+								</Link>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/pages/sign-up-in/Register.jsx
+++ src/pages/sign-up-in/Register.jsx
@@ -1,9 +1,66 @@
+import Lottie from 'lottie-react';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import register from '../../assets/register.json';
 
 const SignUp = () => {
 	return (
-		<div>
-			<h1>SignUp</h1>
+		<div className='hero mt-10'>
+			<div className='hero-content flex-col lg:flex-row gap-20'>
+				<div>
+					<Lottie className='h-96' animationData={register} loop={true} />
+				</div>
+				<div className='card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
+					<div className='card-body'>
+						<div className='form-control'>
+							<input
+								type='text'
+								placeholder='Name'
+								className='input input-bordered'
+								name='name' required
+							/>
+						</div>
+						<div className='form-control'>
+							<input
+								type='email'
+								placeholder='Email'
+								className='input input-bordered'
+								name='email' required
+							/>
+						</div>
+						<div className='form-control'>
+							<input
+								type='password'
+								placeholder='Password'
+								className='input input-bordered'
+								name='password' required
+							/>
+						</div>
+						<div className='form-control'>
+							<input
+								type='text'
+								placeholder='Photo URL'
+								className='input input-bordered'
+								name='photoUrl' required
+							/>
+						</div>
+						<div className='form-control mt-6'>
+							<button className='btn btn-primary'>Register</button>
+						</div>
+						<div className='mt-3'>
+							<p>
+								Already have an account?{' '}
+								<Link
+									className='Active transform active:scale-90 transition-transform'
+									to='/login'
+								>
+									Login
+								</Link>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
